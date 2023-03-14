47c1371424dd1592b3c917596b02f3dc891dca5f
ksadirac <khan.sadirac@ubisoft.com>
2023-03-15 00:18:24 +0100

    update metro move

--- Assets/Scripts/MetroMgr.cs
+++ Assets/Scripts/MetroMgr.cs
@@ -1,11 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.PlasticSCM.Editor.WebApi;
-using Unity.VisualScripting;
-using UnityEditor.Build.Content;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 public class MetroMgr : MonoBehaviour
@@ -13,9 +7,11 @@ public class MetroMgr : MonoBehaviour
     GameObject metro;
     GameObject GameManager;
     Generator generator;
+    public Boolean isMoving = false;
+    enum Direction { LEFT, RIGHT };
 
-    Boolean isMoving = false;
-
+    Vector3 lastPosition;
+    Vector3 onGoingPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -23,112 +19,211 @@ public class MetroMgr : MonoBehaviour
         GameManager = GameObject.Find("GameManager");
         generator = GameManager.GetComponent<Generator>();
         metro = gameObject;
+        lastPosition = metro.transform.position;
     }
 
-    private string getMetroCurrentStation(Vector3 currentPos) {
-        if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position) {
+    private string getMetroCurrentStation(Vector3 currentPos)
+    {
+        if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position)
+        {
             return "Top";
-        } else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position) {
+        }
+        else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position)
+        {
             return "Bottom";
-        } else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position) {
+        }
+        else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position)
+        {
             return "Left";
-        } else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position) {
+        }
+        else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position)
+        {
             return "Right";
-        } else {
+        }
+        else
+        {
             return "Unknown";
         }
     }
 
-    private Vector3 getNextMetroStationPos(Vector3 currentPos, String direction) {
-        Debug.Log(getMetroCurrentStation(currentPos));
-
-        if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position) {
-            if (direction == "LEFT") {
-                return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
-            } else {
-                return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+    private Vector3 getNextMetroStationPos(Direction direction)
+    {
+        if (direction == Direction.LEFT)
+        {
+            // LastPosition == Top Station
+            if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position)
+            {
+                if (metro.transform.position.x == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+                else if (metro.transform.position.x > generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+                else
+                    return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
             }
-        } else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position) {
-            if (direction == "LEFT") {
-                return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
-            } else {
-                return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+            else if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position)
+            {
+                if (metro.transform.position.x == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+                else if (metro.transform.position.x < generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+                else
+                    return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
             }
-        } else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position) {
-            if (direction == "LEFT") {
-                return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
-            } else {
-                return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+            else if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position)
+            {
+                if (metro.transform.position.z == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+                }
+                else if (metro.transform.position.z > generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+                }
+                else
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+                }
             }
-        } else if (currentPos == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position) {
-            if (direction == "LEFT") {
-                return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
-            } else {
-                return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+            else if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position)
+            {
+                if (metro.transform.position.z == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+                }
+                else if (metro.transform.position.z < generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+                }
+                else
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+                }
             }
-        } else {
-            return currentPos;
         }
-        
-        // @TODO: Better generation
-        // String heightPos = currentPos.z > ((generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position.z + generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position.z))/2 ? "T" : "B";
-        // String widthPos = currentPos.x > ((generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position.x + generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position.x))/2 ? "R" : "L";
+        else if (direction == Direction.RIGHT)
+        {
+            // LastPosition == Top Station
+            if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position)
+            {
+                if (metro.transform.position.x == generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+                else if (metro.transform.position.x < generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+                else
+                    return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+            }
+            else if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position)
+            {
+                if (metro.transform.position.x == generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+                else if (metro.transform.position.x > generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position.x)
+                    return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+                else
+                    return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+            }
+            else if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position)
+            {
+                if (metro.transform.position.z == generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+                }
+                else if (metro.transform.position.z < generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+                }
+                else
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+                }
+            }
+            else if (lastPosition == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position)
+            {
+                if (metro.transform.position.z == generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+                }
+                else if (metro.transform.position.z > generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position.z)
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+                }
+                else
+                {
+                    return generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+                }
+            }
+        }
 
-        // if (heightPos == "T") {
-        //     if (widthPos == "R") {
-        //         // top (exclu) -> Right (exclu)
-        //     } else {
-        //         // Left (exclu) -> Top (inclu)
-        //     }
-        // } else {
-        //     if (widthPos == "R") {
-        //         // Right (Inclu) -> Bottom (inclu)
-        //     } else {
-        //         // Left (inclu) -> botton (exclu)
-        //     }
-        // }
+        return generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
     }
 
-    private void MoveLeft() {
-        if (isMoving) {
-            return ;
+    IEnumerator MoveToPosition()
+    {
+        while (metro.transform.position != onGoingPosition && isMoving)
+        {
+            metro.transform.position = Vector3.MoveTowards(metro.transform.position, onGoingPosition, Time.deltaTime * 50f);
+            yield return null;
         }
-
-        Vector3 newPos = getNextMetroStationPos(metro.transform.position, "LEFT");
-
-        isMoving = true;
-
-        metro.transform.position = newPos;
-
+        lastPosition = onGoingPosition;
         isMoving = false;
     }
 
-    private void MoveRight() {
-        if (isMoving) {
-            return ;
+    private void Move(Direction direction)
+    {
+        if (isMoving)
+        {
+            return;
         }
 
-        Vector3 newPos = getNextMetroStationPos(metro.transform.position, "RIGHT");
-
+        onGoingPosition = getNextMetroStationPos(direction);
         isMoving = true;
 
-        metro.transform.position = newPos;
+        StartCoroutine(MoveToPosition());
+    }
 
+    private void Stop()
+    {
         isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J)) {
-            Debug.Log("Moving left");
-            MoveLeft();   
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Move(Direction.LEFT);
+            Debug.Log("LEFT -> " + getMetroCurrentStation(metro.transform.position));
         }
 
-        if (Input.GetKeyDown(KeyCode.L)) {
-            Debug.Log("Moving right");
-            MoveRight();
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Move(Direction.RIGHT);
+            Debug.Log("AFTER RIGHT -> " + getMetroCurrentStation(metro.transform.position));
         }
 
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("Stop");
+            Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            metro.transform.position = generator.metroStationPrefabs[Generator.MetroStation.Top].transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            metro.transform.position = generator.metroStationPrefabs[Generator.MetroStation.Bottom].transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            metro.transform.position = generator.metroStationPrefabs[Generator.MetroStation.Left].transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            metro.transform.position = generator.metroStationPrefabs[Generator.MetroStation.Right].transform.position;
+        }
     }
 }
